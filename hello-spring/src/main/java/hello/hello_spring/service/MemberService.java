baf4043930f0c6977abde1d8332b8f4d9e031862
seomj <smj100394@gmail.com>
2025-10-28 09:23:38 +0900

    스프링 입문 - 3. 회원 관리 예제

---
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,39 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복된 회원이 있는지 확인
+        memberRepository.save(member); // 회원 저장
+        return member.getId(); // 회원의 ID 반환
+    }
+
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName()) // 이름으로 회원 검색
+                .ifPresent(m -> { // 중복된 회원이 있으면 예외 발생
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
