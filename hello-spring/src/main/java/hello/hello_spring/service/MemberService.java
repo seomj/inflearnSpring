786df557648377a67fb91492bcd1d11619a94936
seomj <smj100394@gmail.com>
2025-11-06 13:19:09 +0900

    스프링 입문 - 7. AOP

--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -22,9 +22,18 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member){
-        validateDuplicateMember(member); // 중복된 회원이 있는지 확인
-        memberRepository.save(member); // 회원 저장
-        return member.getId(); // 회원의 ID 반환
+
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); // 중복된 회원이 있는지 확인
+            memberRepository.save(member); // 회원 저장
+            return member.getId(); // 회원의 ID 반환
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member){
@@ -38,7 +47,17 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
+
     }
 
     public Optional<Member> findOne(Long memberId){
